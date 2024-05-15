8e636f98f8e2168bd6a14d281a5470eda0a2aff4
kulikp1 <pashakylik2@gmail.com>
2024-05-15 17:18:53 +0300

    change profile file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "modern-normalize"
+// import "modern-normalize"
 import App from './components/App/App'
 import './index.css'
 
